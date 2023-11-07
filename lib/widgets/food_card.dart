e0911f62eb2d477747df98b43e85910d6d234f66
Victor Smaniotto <victorsmaniotto@hotmail.com>
2023-11-06 23:27:55 -0300

    Tela da receita finalizada

--- lib/widgets/food_card.dart
+++ lib/widgets/food_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_app/models/food.dart';
+import 'package:food_app/screens/recipe_screen.dart';
 import 'package:iconsax/iconsax.dart';
 
 class FoodCard extends StatelessWidget {
@@ -8,114 +9,122 @@ class FoodCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: double.infinity,
-                height: 100,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  image: DecorationImage(
-                      image: AssetImage(food.image), fit: BoxFit.fill),
-                ),
-              ),
-              const SizedBox(height: 5),
-              Text(
-                food.name,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Row(
-                children: [
-                  const Icon(
-                    Iconsax.flash_1,
-                    size: 18,
-                    color: Colors.grey,
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => RecipeScreen(food: food),
+        ),
+      ),
+      child: SizedBox(
+        width: double.infinity,
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    image: DecorationImage(
+                        image: AssetImage(food.image), fit: BoxFit.fill),
                   ),
-                  Text(
-                    "${food.cal} Cal",
-                    style: const TextStyle(
-                      fontSize: 12,
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  food.name,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Row(
+                  children: [
+                    const Icon(
+                      Iconsax.flash_1,
+                      size: 18,
                       color: Colors.grey,
                     ),
-                  ),
-                  const Text(
-                    " • ",
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                  const Icon(
-                    Iconsax.clock,
-                    size: 18,
-                    color: Colors.grey,
-                  ),
-                  Text(
-                    "${food.time} Min",
-                    style: const TextStyle(
-                      fontSize: 12,
+                    Text(
+                      "${food.cal} Cal",
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const Text(
+                      " • ",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    const Icon(
+                      Iconsax.clock,
+                      size: 18,
                       color: Colors.grey,
                     ),
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Iconsax.star5,
-                    color: Colors.yellow.shade600,
-                    size: 18,
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  Text(
-                    "${food.rate}/5",
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.grey.shade600,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  Text(
-                    "(${food.reviews} Reviews)",
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.grey.shade600,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          Positioned(
-            top: 1,
-            right: 1,
-            child: IconButton(
-              onPressed: () {
-                //
-              },
-              style: IconButton.styleFrom(
-                backgroundColor: Colors.white,
-                fixedSize: const Size(30, 30),
-              ),
-              iconSize: 20,
-              icon: food.isLiked
-                  ? const Icon(
-                      Iconsax.heart5,
-                      color: Colors.red,
+                    Text(
+                      "${food.time} Min",
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
                     )
-                  : const Icon(Iconsax.heart),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Iconsax.star5,
+                      color: Colors.yellow.shade600,
+                      size: 18,
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      "${food.rate}/5",
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey.shade600,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      "(${food.reviews} Reviews)",
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey.shade600,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          )
-        ],
+            Positioned(
+              top: 1,
+              right: 1,
+              child: IconButton(
+                onPressed: () {
+                  //
+                },
+                style: IconButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  fixedSize: const Size(30, 30),
+                ),
+                iconSize: 20,
+                icon: food.isLiked
+                    ? const Icon(
+                        Iconsax.heart5,
+                        color: Colors.red,
+                      )
+                    : const Icon(Iconsax.heart),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
