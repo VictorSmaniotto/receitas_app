2a2490219ee08c0eda59ad5b3f8dc6e7b0edbea9
Victor Smaniotto <victorsmaniotto@hotmail.com>
2023-11-06 22:17:38 -0300

    Fiz a separação dos componentes e preparei o link do view all

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:food_app/constants.dart';
-import 'package:food_app/models/category.dart';
-import 'package:food_app/models/food.dart';
 import 'package:food_app/widgets/categories.dart';
 import 'package:food_app/widgets/home_appbar.dart';
 import 'package:food_app/widgets/home_searchbar.dart';
 import 'package:food_app/widgets/quick_and_fast.dart';
-import 'package:iconsax/iconsax.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
